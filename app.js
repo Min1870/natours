77a717c18a51713f6ab184ebd5e015b275c05d77
Min1870 <minkhantzaw.peti@gmail.com>
2023-12-21 19:37:24 +0630

    Refactor: Error Handling

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const morgan = require('morgan');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
 
-console.log(process.env.NODE_ENV)
+console.log(process.env.NODE_ENV);
 
 // 1) MIDDLEWARE
 if (process.env.NODE_ENV === 'development') {
@@ -14,13 +16,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 app.use(express.json());
-
 app.use(express.static(`${__dirname}/public`));
-
-app.use((req, res, next) => {
-  console.log('Hello from the middleware!!!!');
-  next();
-});
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -31,5 +27,11 @@ app.use((req, res, next) => {
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
