653b74a328ce2d6709e2b995901947304d763fc1
minkhantzaw <minkhantzaw@g.com>
2024-01-29 21:40:25 +0630

    add data sanitization

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -32,7 +34,13 @@ const limiter = rateLimit({
 app.use('/api', limiter);
 
 // Body Parser, reading data from body into req.body
-app.use(express.json({limit: '10kb'}));
+app.use(express.json({ limit: '10kb' }));
+
+// Data Sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data Sanitization against XSS
+app.use(xss())
 
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
